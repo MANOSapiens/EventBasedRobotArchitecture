eada418e048610d45b04922f0276c974fa5e74d4
root <root@DESKTOP-TGRRA5P>
2023-12-19 21:38:10 +0100

    first commit

--- src/main.rs
+++ src/main.rs
@@ -2,44 +2,26 @@
 extern crate ev3dev_lang_rust;
 extern crate image;
 extern crate imageproc;
-use glob::glob;
+
 use image::Rgb;
 use std::path::{Path};
 use rusttype::{Font, Scale};
 
-use ev3dev_lang_rust::motors::{MotorPort};
-use ev3dev_lang_rust::sensors::{SensorPort};
 use ev3dev_lang_rust::Screen;
 use ev3dev_lang_rust::Button;
+use ev3dev_lang_rust::motors::{MediumMotor, LargeMotor, MotorPort};
+use ev3dev_lang_rust::sensors::{SensorPort, ColorSensor, GyroSensor};
 
-// Local modules
-pub mod Events;
-mod Ports;
-use crate::Ports::PortDefinition;
 
-mod Logger;
-use crate::Logger::init_logger;
+pub struct PortDefinition {
+    pub lDriveMotorPort: MotorPort,
+    pub rDriveMotorPort: MotorPort,
+    pub lToolMotorPort: MotorPort,
+    pub rToolMotorPort: MotorPort,
+}
 
-// Function Definition
-mod Actuators;
-mod Check;
-mod PID;
-mod ProcessLoop;
-mod ReadInstructions;
-mod ReadSensors;
-mod RunEvents;
-mod SpawnTerminateEvents;
-mod StartExecution;
-pub mod consts;
-
-use crate::consts::*;
-use crate::StartExecution::startExecution;
 
 fn main() {
-    // Initialize logger
-    let logger_config_file = String::from("log/log4rs.yaml");
-    init_logger(logger_config_file);
-
     let mut screen = Screen::new().unwrap();
     let button = Button::new().expect("buttons cannot be loaded");
 
@@ -52,56 +34,152 @@ fn main() {
         lToolMotorPort: MotorPort::OutD,
         rToolMotorPort: MotorPort::OutA,
         gyroSensPort: SensorPort::In1,
-        colourSensPort: SensorPort::In4,
     };
 
-    let mut paths = Vec::new();
-    let mut filenames = Vec::new();
-    let mut index: usize = 0;
+    let lDriveMotor = LargeMotor::get(port_definitions.lDriveMotorPort).expect("failed to load lDriveMotor");
+    let rDriveMotor = LargeMotor::get(port_definitions.rDriveMotorPort).expect("failed to load rDriveMotor");
+    let lToolMotor = MediumMotor::get(port_definitions.lToolMotorPort).expect("failed to load lToolMotor");
+    let rToolMotor = MediumMotor::get(port_definitions.rToolMotorPort).expect("failed to load rToolMotor");
+    let gyroSens = GyroSensor::get(port_definitions.gyroSensPort).expect("failed to load gyroSens");
+    gyroSens.set_mode_gyro_ang().expect("failed to set gyroSens to gyro ang mode");
 
-    for entry in glob("instructions/*.json").expect("Failed to read glob pattern") {
-        match entry {
-            Ok(path_buf) => {
-                let path: &Path = path_buf.as_ref();
-
-                // Convert &Path to &str
-                let path_str: &str = path.to_str().unwrap();
-                paths.push(path_str.to_string());
-
-                let l1: &str = path_str.split("/").nth(1).unwrap();
-                let l2: &str = l1.split(".").nth(0).unwrap();
-                filenames.push(l2.to_string())
-            },
-            Err(e) => println!("{:?}", e),
-        }
-    }
+    let _ = lToolMotor.run_direct(); //SET RUN DIRECT MODE
+    let _ = rToolMotor.run_direct(); //SET RUN DIRECT MODE
     
+    let mut screenSelection: i8 = 0;
+    let mut lDriveMotorEncoder: i32 = 0;
+    let mut rDriveMotorEncoder: i32 = 0;
+    let mut lToolMotorEncoder: i32 = 0;
+    let mut rToolMotorEncoder: i32 = 0;
+    let mut gyroSensValue: i32 = 0;
+
+    // tool motor power for every motor
+    let mut lToolMotorPower: i32 = 0;
+    let mut rToolMotorPower: i32 = 0;
+    let mut toolMotorPower: i32 = 50;
+
+
     loop {
         button.process();
-        if button.is_backspace() {
-            break;
-        }
 
+        // switch between 3 screenSelections by pressing the enter button
         if button.is_enter() {
-            startExecution(&*paths.get(index).expect("index out of bounds"), &port_definitions);
+            screenSelection += 1;
+            if screenSelection > 2 {
+                screenSelection = 0;
+            }
+            // reset all encoders to zero
+            lDriveMotor.set_position(0).expect("failed to reset lDriveMotor encoder");
+            rDriveMotor.set_position(0).expect("failed to reset rDriveMotor encoder");
+            lToolMotor.set_position(0).expect("failed to reset lToolMotor encoder");
+            rToolMotor.set_position(0).expect("failed to reset rToolMotor encoder");
+
+            std::thread::sleep(std::time::Duration::from_millis(150));
         }
-
-        if button.is_right() && index < paths.len()-1{
-            index += 1;
-            std::thread::sleep(std::time::Duration::from_millis(250));
-        }
-
-        if button.is_left() && index > 0{
-            index -= 1;
-            std::thread::sleep(std::time::Duration::from_millis(250));
-        }
-
-
-        screen.clear();
-        imageproc::drawing::draw_text_mut(&mut screen.image, Rgb([0, 0, 0]), 0, 20, Scale::uniform(25.0), &font, filenames.get(index).expect("index out of bounds"));
-        screen.update();
         
+        // switch case for the 3 screenSelections
+        // reset both power to zero
+        lToolMotorPower = 0;
+        rToolMotorPower = 0;
+        
+
+        // read encoders of all motors
+        lDriveMotorEncoder = lDriveMotor.get_position().expect("failed to read lDriveMotor encoder");
+        rDriveMotorEncoder = rDriveMotor.get_position().expect("failed to read rDriveMotor encoder");
+        lToolMotorEncoder = lToolMotor.get_position().expect("failed to read lToolMotor encoder");
+        rToolMotorEncoder = rToolMotor.get_position().expect("failed to read rToolMotor encoder");
+
+        match screenSelection {
+            0 => {
+
+                screen.clear();
+                imageproc::drawing::draw_text_mut(&mut screen.image, Rgb([0, 0, 0]), 30, 0, Scale::uniform(25.0), &font, "TOOLMOTOR");
+                
+                // draw the power of the tool motors
+
+                imageproc::drawing::draw_text_mut(&mut screen.image, Rgb([0, 0, 0]), 0, 15, Scale::uniform(50.0), &font, &lToolMotorEncoder.to_string());
+                imageproc::drawing::draw_text_mut(&mut screen.image, Rgb([0, 0, 0]), 100, 15, Scale::uniform(50.0), &font, &rToolMotorEncoder.to_string());
+                
+                //imageproc::drawing::draw_text_mut(&mut screen.image, Rgb([0, 0, 0]), 20, 80, Scale::uniform(15.0), &font, "LEFT(CW)");
+                //imageproc::drawing::draw_text_mut(&mut screen.image, Rgb([0, 0, 0]), 20, 100, Scale::uniform(15.0), &font, "RIGHT(CCW)");
+
+                //imageproc::drawing::draw_text_mut(&mut screen.image, Rgb([0, 0, 0]), 115, 80, Scale::uniform(15.0), &font, "UP(CW)");
+                //imageproc::drawing::draw_text_mut(&mut screen.image, Rgb([0, 0, 0]), 115, 100, Scale::uniform(15.0), &font, "DOWN(CCW)");
+                
+                screen.update();
+
+                // if button pressed assign toolMotorPower to the power of the tool motors
+                if button.is_up() {
+                    rToolMotorPower = toolMotorPower;
+                }
+                if button.is_down() {
+                    rToolMotorPower = -toolMotorPower;
+                }
+                if button.is_left() {
+                    lToolMotorPower = -toolMotorPower;
+                }
+                if button.is_right() {
+                    lToolMotorPower = toolMotorPower;
+                }
+
+                // write power to tool motors
+                lToolMotor.set_duty_cycle_sp(lToolMotorPower).expect("lToolMotor write failed");
+                rToolMotor.set_duty_cycle_sp(rToolMotorPower).expect("rToolMotor write failed");
+            },
+            1 => {
+                // button up increases the power of the tool motors up to a maximum of 1 in 0.1 steps
+                if button.is_up() {
+                    toolMotorPower += 10;
+                    if toolMotorPower > 100 {
+                        toolMotorPower = 100;
+                    }
+                    //timer
+                    std::thread::sleep(std::time::Duration::from_millis(150));
+                }
+
+                // button down decreases the power of the tool motors down to a minimum of 0 in 0.1 steps
+                if button.is_down() {
+                    toolMotorPower -= 10;
+                    if toolMotorPower < 0 {
+                        toolMotorPower = 0;
+                    }
+                    //timer
+                    std::thread::sleep(std::time::Duration::from_millis(150));
+                }
+
+                screen.clear();
+                imageproc::drawing::draw_text_mut(&mut screen.image, Rgb([0, 0, 0]), 25, 0, Scale::uniform(25.0), &font, "SELECT POWER");
+                imageproc::drawing::draw_text_mut(&mut screen.image, Rgb([0, 0, 0]), 50, 20, Scale::uniform(100.0), &font, &toolMotorPower.to_string());
+                screen.update();
+            },
+            2 => {
+                gyroSensValue = gyroSens.get_angle().expect("failed to read gyroSens");
+                screen.clear();
+                imageproc::drawing::draw_text_mut(&mut screen.image, Rgb([0, 0, 0]), 28, 0, Scale::uniform(25.0), &font, "DRIVEMOTOR");
+                
+                // draw the power of the tool motors
+
+                imageproc::drawing::draw_text_mut(&mut screen.image, Rgb([0, 0, 0]), 0, 15, Scale::uniform(50.0), &font, &lDriveMotorEncoder.to_string());
+                imageproc::drawing::draw_text_mut(&mut screen.image, Rgb([0, 0, 0]), 100, 15, Scale::uniform(50.0), &font, &rDriveMotorEncoder.to_string());
+                imageproc::drawing::draw_text_mut(&mut screen.image, Rgb([0, 0, 0]), 50, 50, Scale::uniform(50.0), &font, &gyroSensValue.to_string());
+                
+                //imageproc::drawing::draw_text_mut(&mut screen.image, Rgb([0, 0, 0]), 20, 80, Scale::uniform(15.0), &font, "LEFT(CW)");
+                //imageproc::drawing::draw_text_mut(&mut screen.image, Rgb([0, 0, 0]), 20, 100, Scale::uniform(15.0), &font, "RIGHT(CCW)");
+
+                //imageproc::drawing::draw_text_mut(&mut screen.image, Rgb([0, 0, 0]), 115, 80, Scale::uniform(15.0), &font, "UP(CW)");
+                //imageproc::drawing::draw_text_mut(&mut screen.image, Rgb([0, 0, 0]), 115, 100, Scale::uniform(15.0), &font, "DOWN(CCW)");
+                
+                screen.update();
+            },
+            _ => {
+            }
+        }
+
+        
+
     }
+
+    
 
     
 }
